dc907ad6b21445cd4029b824dacba632046da760
Worachet Nimcharoen <43021938+WorachetNimcharoen@users.noreply.github.com>
2021-12-12 18:06:30 +0700

    Update
    Update API

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ def get_covid19_report(url):
 url = 'https://covid19.ddc.moph.go.th/api/Cases/timeline-cases-all'
 get_covid19_report(url)
 
+
+
 with open('data.json') as f:
     raw = json.load(f)
     df = pd.read_json('data.json')
 
-
+getdataprovince = requests.get("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces")
 response = requests.get("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all")
+getdatadender = requests.get("https://covid19.ddc.moph.go.th/api/Cases/round-3-line-lists")
 
 getdata = requests.get("https://covid19.ddc.moph.go.th/api/Cases/timeline-cases-all")
 print("Daily report covid")
@@ -33,6 +36,9 @@ total_death = response.json()[0]['total_death']
 new_recovered = response.json()[0]['new_recovered']
 total_recovered = response.json()[0]['total_recovered']
 update_date = response.json()[0]['update_date']
+province = getdataprovince.json()[0]['province']
+Gender = getdatadender.json()['data'][0]['gender']
+
 
 df = pd.read_json('https://covid19.ddc.moph.go.th/api/Cases/timeline-cases-all')
 df.info()
@@ -43,13 +49,15 @@ pd.DataFrame(df)
 def createNewWindow():
     newWindow = tk.Toplevel(app)
     lbl = tk.Label(newWindow,text= "ผู้ป่วยติดเชื้อใหม่ "+ str(newcase)).pack()
-    newWindow.geometry("500x500")
+    newWindow.geometry("1000x1000")
     df['txn_date'] = df['txn_date'].astype('datetime64')
     df['txn_date'].groupby([df['txn_date'].dt.year, df['txn_date'].dt.month]).count().plot()
     plt.xlabel('Time (Year, Month)')
     plt.ylabel('Number of Cases')
     plt.title('Number of Reported Cases Each Month')
     plt.show()
+
+
     
     
 
@@ -64,6 +72,8 @@ label = tk.Label(text= "ผู้เสียชีวิตสะสม "+ str(
 label = tk.Label(text= "ผู้ได้รับการรักษาใหม่ "+ str(new_recovered)).pack()
 label = tk.Label(text= "ผู้ได้รับการรักษาสะสม "+ str(total_recovered)).pack()
 label = tk.Label(text= "วันที่๊ Update ข้อมูล "+ str(update_date)).pack()
+label = tk.Label(text= "จังหวัด "+ str(province)).pack()
+label = tk.Label(text= "เพศ "+ str(Gender)).pack()
 buttonExample = tk.Button(app, 
               text="Graph",
               command=createNewWindow).pack()
